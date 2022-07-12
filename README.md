2087eb2d4e0b92fd9d775055f8bc5c31db321f97
Sonny Brown <68655112+LowLifeArcade@users.noreply.github.com>
2022-07-11 22:06:23 -0700

    Updates on game function

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # * ### Security Failure ### *
 ## System Command Options
 
-  > We need to cancel cron jobs and override diff check teardowns before they corrupt and burn up the data hives!
+  > The systems are in MELTDOWN. We need to cancel cron jobs, override diff checks, and deploy super cooling before they corrupt and burn up the data hives!
   
   #### Your job
   > You're a new hire and training was sparse. Sorry. Open the sequencer and use the `sysCmd` function to send system commands to shut down the systems safely.
   #### How To Succeed
-  > Use `auto complete` by either pressing cmd + i (ctrl + i on PC... I think) or opening a quote. Hover over the function to get some information to go off of.
+  > Use `auto complete` by either pressing cmd + i (ctrl + i on PC... I think) or opening a quote. Hover over the function to get some information to go off of. That'll make up for your training.
 
   #### Extra Credit
-  > Once you safely shut down the system, build guards in the `sysCmd` function against wrong attempts for the future!
+  > Once you safely shut down the systems, build guards in the `sysCmd` function against wrong attempts for the future!
 
   ## Press the period key to start
--- sequencer.js
+++ sequencer.js
@@ -1,17 +1,19 @@
 import secureC from './src/lockedSecureCode/secureC';
 
 /**
- * @param { SysURLs } url Open a pair of quotes for autocomplete.
+ * @param { SysURLs } uri Open a pair of quotes for autocomplete.
  * @param { SysCmdOpts } sysOpts Open an object and press cmd+i to get autocomplete.
  */
-export function sysCmd(url, sysOpts) {
-    secureC(url, sysOpts);
+export function sysCmd(uri, sysOpts) {
+    secureC(uri, sysOpts);
 };
 
 // INSTRUCTIONS - In the sequence block, use the sysCmd function to:
-// 1. deploy a red level arg disconnect of type kill with a "no value unsafe hard stash kill" to the danger-access path.
-// 2. proliferate safe blue level arg deployments of a "true value directory reset" to the secure path.
-// 3. finally use a silver level arg disconnect type safe command to spin down the overclock on the root path.
+// 1. SSH into blade vault
+// 2. run an over ride command to cool down the system through the debug path
+// 3. deploy a red level arg disconnect of type kill with a "no value unsafe hard stash kill" to the danger-access path.
+// 4. proliferate safe blue level arg deployments of a "Peter Thiemin Lendruf value directory reset" to the secure path.
+// 5. finally use a silver level arg disconnect type safe command to spin down the overclock on the root path.
 
 
 
@@ -22,7 +24,6 @@ export function sysCmd(url, sysOpts) {
 
 // -----------------
 //
-//
 // SYSCMD 2.)
 // -----------------
 
@@ -30,6 +31,18 @@ export function sysCmd(url, sysOpts) {
 //
 //
 // SYSCMD 3.)
+// -----------------
+
+// -----------------
+//
+//
+// SYSCMD 4.)
+// -----------------
+
+// -----------------
+//
+//
+// SYSCMD 5.)
 // -----------------
 
 // -----------------
@@ -99,13 +112,14 @@ export function sysCmd(url, sysOpts) {
 /**
  *  @typedef {{
  *      type: "KILL" | "GET" | "RUN" | "DEBUG" | "SAFE",
+ *      loc_encrypt_conn: "SSH" | "SEC_URI" | "pass"
  *      deploy: boolean,
  *      cmd: "sys" | "orgLvl" | "trans" | "ex" | "proliferate" | "disconn" ,
  *      argLvl: "blue" | "red" | "silver" | "green",
- *      sysCmd: { val: boolean, cur: '$__use_cron' | '$__ex_loput_cancel' | '$__dir__reset' | '$__immediate_KILL' | '$__unsafe_HARD_stash_kill'}
+ *      sysCmd: { val: "over_ride" | "ptl" | "turn_key" | "no_val", cur: '$__use_cron' | '$__sys_cooldown' | '$__ex_loput_cancel' | '$__dir__reset' | '$__immediate_KILL' | '$__unsafe_HARD_stash_kill'}
  *  }} SysCmdOpts
  */
 
 /**
- * @typedef {'/' | '/secure' | '/debug' | '/unsecure' | '/danger-access'} SysURLs
+ * @typedef {'/' | '/secure' | '/debug' | '/unsecure?conn=love%20less&nopass=silent' | '/danger-access' | '/reden/sandepss/blade-vault'} SysURLs
  */
